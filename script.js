2edc796a0b56732a6d70da54cd46c1bc8a5330c0
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-27 12:43:55 +0700

    Add yscale xscale

--- script.js
+++ script.js
@@ -3,24 +3,53 @@
 // Our canvas
 const width = 750,
   height = 300,
-  margin = 20
-marginLeft = 40
+  margin = 20,
+  marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
+  .style('background', '#cacaca')
+  .attr('fill', 'green')
 
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  let data = []
+  d3.tsv('afcw-results.tsv', (rows) => {
+    rows.forEach(function (score) {
+      data.push(score.GoalsScored)
+    })
+    redraw(data)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
-
+  yScale = d3.scaleLinear()
+    .domain([0, d3.max(d3.values(data))])
+    .range([0, height])
+  xScale = d3.scaleLinear()
+    .domain([0, data.length])
+    .range([0, width])
+  svg.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d, index) => {
+      return xScale(index)
+    })
+    .attr('y', (d) => {
+      return height - yScale(d)
+    })
+    .attr('width', 15)
+    .attr('height', (d) => {
+      return yScale(d)
+    })
 }
 
 reload()
